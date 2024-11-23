39e31f311ac5b0e5e9ca61f986f8f57895dfe87c
Нес <90711230+Hecmia@users.noreply.github.com>
2024-11-23 15:50:29 +0300

    Delete tg_bot directory

--- tg_bot/keyboards/for_do_you_know_teachers.py
+++
@@ -1,9 +0,0 @@
-from aiogram.types import ReplyKeyboardMarkup
-from aiogram.utils.keyboard import ReplyKeyboardBuilder
-
-def get_kb_do_you_know_teacher()->ReplyKeyboardMarkup:
-    kb = ReplyKeyboardBuilder()
-    kb.button(text="Я знаю преподавателя")
-    kb.button(text="Я не знаю преподавателя")
-    kb.adjust(2)
-    return kb.as_markup(resize_keyboard=True)
